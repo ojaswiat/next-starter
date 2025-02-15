03e378e9a244d3e9361b570a3a73f7eab96ba326
Ojaswi Athghara <ojaswi@acalvio.com>
2025-02-15 20:47:40 +0530

    verified user signup via email

---
+++ lib/constants.ts
@@ -0,0 +1,24 @@
+export const CLIENT_ROUTES = {
+    HOME: "/",
+
+    // app
+    DASHBOARD: "/dashboard",
+
+    // auth
+    LOGIN: "/login",
+
+    REGISTER: "/register",
+    REGISTER_CONFIRMATION: "/register/confirmation",
+
+    FORGOT_PASSWORD: "/forgot-password",
+    FORGOT_PASSWORD_CONFIRMATION: "/forgot-password/confirmation",
+    FORGOT_PASSWORD_RESET_PASSWORD: "/forgot-password/reset-password",
+
+    // error
+    ERROR: "/error",
+};
+
+export const SERVER_ROUTES = {
+    // auth
+    AUTH: "/auth",
+};
